c8ff696f9b858bcda04b548872ed4f6564b8880f
GalDrnovsek <gal.drnovsek@gmail.com>
2017-06-05 18:31:15 +0200

    Update fuzbal.r

--- fuzbal.r
+++ fuzbal.r
@@ -10,6 +10,7 @@ tab3 <- stran3 %>% html_nodes(xpath ="//table[1]") %>% .[[4]]
 tabela3 <- tab3 %>% html_table()
 
 ekipe_link <- tab3 %>% html_nodes(xpath=".//a") %>% html_attr("href")
+
 url4 <- "http://www.betstudy.com/soccer-stats/teams/chelsea/661/squad/"
 stran4 <- html_session(url4) %>% read_html(encoding="UTF-8")
 tabela_igralcev <- stran4 %>% html_nodes(xpath ="//table[1]") %>% .[[2]] %>% html_table()
@@ -33,5 +34,26 @@ names(tabela_igralcev)[8] <- "YCards"
 names(tabela_igralcev)[9] <- "SecYCards"
 names(tabela_igralcev)[10] <- "RCards"
 names(tabela_igralcev)[12] <- "Goals"
+
+
+url <- "http://www.betstudy.com/soccer-stats/teams/chelsea/661/transfers/"
+stran <- html_session(url) %>% read_html(encoding="cp1250")
+tabela_transferjev <- stran %>% html_nodes(xpath ="//table[1]") %>% .[[2]] %>% html_table()
+Encoding(tabela_transferjev[,2]) <- "UTF-8"
+tabela_transferjev$Ekipa <- "Chelsea"
+
+
+for(url in ekipe_link){
+  url <- paste("http://www.betstudy.com", url, sep="")
+  url <- paste(url, "transfers/", sep="")
+  stran <- html_session(url) %>% read_html(encoding="cp1250")
+  tabela <- stran %>% html_nodes(xpath ="//table[1]") %>% .[[2]] %>% html_table()
+  Encoding(tabela[,2]) <- "UTF-8"
+  Encoding(tabela[,4]) <- "UTF-8"
+  tabela$Ekipa <- substr(url, 44, nchar(url)-15)
+  tabela_transferjev <- rbind(tabela_transferjev, tabela)
+}
+
+
 
 
