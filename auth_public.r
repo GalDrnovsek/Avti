356858e12eafd3c340b06a4ab64ac93f2eb3e2ef
GalDrnovsek <gal.drnovsek@gmail.com>
2017-06-08 23:55:33 +0200

    Create auth_public.r

--- auth_public.r
+++ auth_public.r
@@ -2,3 +2,13 @@ db = 'sem2017_janp'
 host = 'baza.fmf.uni-lj.si'
 user = 'javnost'
 password = 'javnogeslo'
+
+db = 'sem2017_gald'
+host = 'baza.fmf.uni-lj.si'
+user = 'javnost'
+password = 'javnogeslo'
+
+db = 'sem2017_zant'
+host = 'baza.fmf.uni-lj.si'
+user = 'javnost'
+password = 'javnogeslo'
