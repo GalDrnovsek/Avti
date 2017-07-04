15ccebe0972f9e093fcbb72ebb67c2e9c5b78bf1
GalDrnovsek <gal.drnovsek@gmail.com>
2017-07-04 15:44:13 +0200

    Delete pomozne stvari.R

--- pomozne stvari.R
+++
@@ -1,5 +0,0 @@
-url <- "http://www.betstudy.com/soccer-stats/teams/chelsea/661/squad/"
-stran <- html_session(url) %>% read_html(encoding="UTF-8")
-tab <- stran %>% html_nodes(xpath ="//table[1]") %>% .[[2]]
-tabela <- tab %>% html_table()
-igralci_link <- tab %>% html_nodes(xpath=".//a") %>% html_attr("href")
