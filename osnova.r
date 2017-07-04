51d022cb9af9f4091a0ba8fd5e2db8b693538272
GalDrnovsek <gal.drnovsek@gmail.com>
2017-07-04 15:43:02 +0200

    Delete osnova.r

--- osnova.r
+++
@@ -1,8 +0,0 @@
-require(dplyr)
-
-# Priklop na bazo. Vnesite ustrezno geslo.
-geslo <- "" 
-
-db <- src_postgres(host="baza.fmf.uni-lj.si", 
-                   dbname = "banka", user = "janp", 
-                   password = geslo)
