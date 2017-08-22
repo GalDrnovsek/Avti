5e9c249f969943c5122b42adddbf96b5b036172b
GalDrnovsek <gal.drnovsek@gmail.com>
2017-08-22 14:42:31 +0200

    Update server.R

--- shiny/server.R
+++ shiny/server.R
@@ -4,7 +4,7 @@ library(RPostgreSQL)
 library(datasets)
 
 source("auth_public.r", encoding="UTF-8")
-source("uvoz/uvoz.r", encoding="UTF-8")
+
 
 shinyServer(
   server <- function(input, output){
@@ -14,25 +14,95 @@ shinyServer(
                          user = user, password = password)
     
     
+    tbl.ekipa <- tbl(conn, "tabelaekipa")
+    tbl.igralcev <- tbl(conn, "tabelaigralcev")
+    tbl.tekma <- tbl(conn, "tabelatekma")
+    tbl.vodstvo <- tbl(conn, "tabelavodstvo")
+    
     
     output$ime1 <- renderText({c(paste0("Ime ekipe: ", input$ekipa1))})
-    output$mesto1 <- renderText({paste0("Mesto: ",as.character(tabelaekipa %>% filter(ekipa %in% input$ekipa1)%>% select(mesto)))})
-    output$stadion1 <- renderText({paste0("Stadion in kapaciteta: ", as.character(tabelaekipa %>% filter(ekipa %in% input$ekipa1)%>% select(stadion)),", ",as.character(tabelaekipa %>% filter(ekipa %in% input$ekipa1)%>% select(kapaciteta)) )})
-    output$manager1 <- renderText({paste0("Manager: ",as.character(tabelavodstvo %>% filter(ekipa %in% input$ekipa1)%>% select(manager)))})
+    
+    output$mesto1 <- renderText({
+      data <- tbl.ekipa %>% filter(ekipa == input$ekipa1) %>% select(mesto) %>%
+        data.frame()
+      paste0("Mesto: ",data$mesto)
+    })
+    
+    output$stadion1 <- renderText({
+      data <- tbl.ekipa %>% filter(ekipa == input$ekipa1) %>% select(stadion, kapaciteta) %>%
+        data.frame()
+      paste0("Stadion in kapaciteta: ", data$stadion,", ",data$kapaciteta )
+    })
+    
+    output$manager1 <- renderText({
+      data <- tbl.vodstvo %>% filter(ekipa == input$ekipa1) %>% select(manager) %>%
+        data.frame()
+      paste0("Manager: ", data$manager)
+    })
+    
+    
     
     output$ime2 <- renderText({c(paste0("Ime ekipe: ", input$ekipa2))})
-    output$mesto2 <- renderText({paste0("Mesto: ",as.character(tabelaekipa %>% filter(ekipa %in% input$ekipa2)%>% select(mesto)))})
-    output$stadion2 <- renderText({paste0("Stadion in kapaciteta: ", as.character(tabelaekipa %>% filter(ekipa %in% input$ekipa2)%>% select(stadion)),", ",as.character(tabelaekipa %>% filter(ekipa %in% input$ekipa2)%>% select(kapaciteta)) )})
-    output$manager2 <- renderText({paste0("Manager: ",as.character(tabelavodstvo %>% filter(ekipa %in% input$ekipa2)%>% select(manager)))})
     
-    output$tabela1 <- renderDataTable({tabelaigralcev %>% filter(ekipa %in% input$ekipa1, pozicija %in% input$pozicija) %>% select(igralec,pozicija,nastopi,podaje,goli)},
-                                      options = list(searching = FALSE, paging = FALSE))
-    output$tabela2 <- renderDataTable({tabelaigralcev %>% filter(ekipa %in% input$ekipa2, pozicija %in% input$pozicija) %>% select(igralec,pozicija,nastopi,podaje,goli)},
-                                      options = list(searching = FALSE, paging = FALSE))
-    output$tekma1 <- renderDataTable({tabelatekma %>% filter(d_ekipa == input$ekipa1 | g_ekipa == input$ekipa1, as.Date(datum,"%d.%m.%Y") > input$zacetek, as.Date(datum,"%d.%m.%Y") < input$konec) %>% select(datum,"Domači"=d_ekipa,"Gostje"=g_ekipa,"Goli domačih"=d_gol,"Goli gostov"=g_gol)},
-                                     options = list(searching = FALSE, paging = FALSE))
-    output$tekma2 <- renderDataTable({tabelatekma %>% filter(d_ekipa == input$ekipa2 | g_ekipa == input$ekipa2, as.Date(datum,"%d.%m.%Y") > input$zacetek, as.Date(datum,"%d.%m.%Y") < input$konec) %>% select(datum,"Domači"=d_ekipa,"Gostje"=g_ekipa,"Goli domačih"=d_gol,"Goli gostov"=g_gol)},
-                                     options = list(searching = FALSE, paging = FALSE))
+    output$mesto2 <- renderText({
+      data <- tbl.ekipa %>% filter(ekipa == input$ekipa2) %>% select(mesto) %>%
+        data.frame()
+      paste0("Mesto: ",data$mesto)
+    })
+    
+    output$stadion2 <- renderText({
+      data <- tbl.ekipa %>% filter(ekipa == input$ekipa2) %>% select(stadion, kapaciteta) %>%
+        data.frame()
+      paste0("Stadion in kapaciteta: ", data$stadion,", ",data$kapaciteta)
+    })
+    
+    output$manager2 <- renderText({
+      data <- tbl.vodstvo %>% filter(ekipa == input$ekipa2) %>% select(manager) %>%
+        data.frame()
+      paste0("Manager: ", data$manager)
+    })
+    
+    
+    
+    output$tabela1 <- renderDataTable({
+      validate(need(!is.null(input$ekipa1), "Izberi prvo ekipo!"))
+      validate(need(length(input$pozicija) >= 1, "Izberi pozicijo!"))
+      pos <- c(input$pozicija, NA) # prisilimo dplyr, da podatke predstavi kot vektor
+      tbl.igralcev %>% filter(ekipa == input$ekipa1, pozicija %in% pos) %>%
+        select(igralec,pozicija,nastopi,podaje,goli) %>% data.frame()
+    }, options = list(searching = FALSE, paging = FALSE, encoding="UTF-8"))
+    
+    
+    output$tabela2 <- renderDataTable({
+      validate(need(!is.null(input$ekipa2), "Izberi drugo ekipo!"))
+      validate(need(length(input$pozicija) >= 1, "Izberi pozicijo!"))
+      pos <- c(input$pozicija, NA) # prisilimo dplyr, da podatke predstavi kot vektor
+      tbl.igralcev %>% filter(ekipa == input$ekipa2, pozicija %in% pos) %>%
+        select(igralec,pozicija,nastopi,podaje,goli) %>% data.frame()
+    }, options = list(searching = FALSE, paging = FALSE, encoding="UTF-8"))
+    
+    
+    
+    output$tekma1 <- renderDataTable({
+      validate(need(!is.null(input$ekipa1), "Izberi prvo ekipo!"))
+      validate(need(length(input$zacetek) == 1 && length(input$konec) == 1,
+                    "Izberi začetni in končni datum!"))
+      tbl.tekma %>% filter(d_ekipa == input$ekipa1 | g_ekipa == input$ekipa1,
+                           datum > input$zacetek, datum < input$konec) %>%
+        select(datum, "Domači" = d_ekipa, "Gostje" = g_ekipa, "Goli domačih" = d_gol,
+               "Goli gostov" = g_gol) %>% data.frame()
+    }, options = list(searching = FALSE, paging = FALSE, encoding="UTF-8"))
+    
+    
+    output$tekma2 <- renderDataTable({
+      validate(need(!is.null(input$ekipa2), "Izberi drugo ekipo!"))
+      validate(need(length(input$zacetek) == 1 && length(input$konec) == 1,
+                    "Izberi začetni in končni datum!"))
+      tbl.tekma %>% filter(d_ekipa == input$ekipa2 | g_ekipa == input$ekipa2,
+                           datum > input$zacetek, datum < input$konec) %>%
+        select(datum, "Domači" = d_ekipa, "Gostje" = g_ekipa, "Goli domačih" = d_gol,
+               "Goli gostov" = g_gol) %>% data.frame()
+    }, options = list(searching = FALSE, paging = FALSE, encoding="UTF-8"))
     
     
   })
